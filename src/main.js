4278cfd92a9f500d07e0f9261ce2a6b65627eefd
RemagOfficial <52126530+RemagOfficial@users.noreply.github.com>
2024-04-16 19:29:27 +0100

    added more formatting options

--- src/main.js
+++ src/main.js
@@ -4,26 +4,40 @@
     const sharkImg = document.getElementById("Shark");
     const formatting = document.getElementById("formatting");
 
-    let standardForm = false;
+    let formattingMode = 0;
 
-    var illions = [" ", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion", "unvigintillion", "duovigintillion", "trevigintillion", "quattuorvigintillion", "quinvigintillion", "sexvigintillion", "septenvigintillion", "octovigintillion", "nonvigintillion", "trigintillion", "untrigintillion", "duotrigintillion", "tretrigintillion", "quattuortrigintillion", "quintrigintillion", "sextrigintillion", "septentrigintillion", "octotrigintillion", "novemtrigintillion", "quadragintillion", "unquadragintillion", "duoquadragintillion", "trequadragintillion", "quattuorquadragintillion", "quinquadragintillion", "sexquadragintillion", "septenquadragintillion", "octoquadragintillion", "novemquadragintillion", "quinquagintillion", "unquinquagintillion", "duoquinquagintillion", "trequinquagintillion"];
+    var illions = ["", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion", "unvigintillion", "duovigintillion", "trevigintillion", "quattuorvigintillion", "quinvigintillion", "sexvigintillion", "septenvigintillion", "octovigintillion", "nonvigintillion", "trigintillion", "untrigintillion", "duotrigintillion", "tretrigintillion", "quattuortrigintillion", "quintrigintillion", "sextrigintillion", "septentrigintillion", "octotrigintillion", "novemtrigintillion", "quadragintillion", "unquadragintillion", "duoquadragintillion", "trequadragintillion", "quattuorquadragintillion", "quinquadragintillion", "sexquadragintillion", "septenquadragintillion", "octoquadragintillion", "novemquadragintillion", "quinquagintillion", "unquinquagintillion", "duoquinquagintillion", "trequinquagintillion"];
+    var illionsAbbr = ["", "k", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "O", "N", "D", "Ud", "DuD", "TrD", "QaD", "QiD", "SxD", "SpD", "OD", "ND", "V", "UnV", "DuV", "TrV", "QaV", "QiV", "SxV", "SpV", "OV", "NV", "Tg", "UnT", "DuT", "TrT", "QaT", "QiT", "SxT", "SpT", "OT", "NT", "Dg", "UnD", "DuDg", "TrDg", "QaDg", "QiDg", "SxDg", "SpDg", "ODg", "NDg", "Sg", "UnSg", "DuSg", "TrSg", "QaSg", "QiSg", "SxSg", "SpSg", "OSg"];
 
     sharkImg.addEventListener("click", addShorks);
     formatting.addEventListener("click", updateFormatting);
 
+    /**
+     * Updates the formatting mode of the application.
+     *
+     * This function increments the formatting mode by 1, and wraps
+     * it around to 0 if it exceeds 3.
+     *
+     * @return {void} This function does not return anything.
+     */
     function updateFormatting() {
-        standardForm = !standardForm;
+        // Increment the formatting mode and wrap it around to 0 if it exceeds 3
+        formattingMode = (formattingMode + 1) % 4;
     }
 
     function addShorks() {
-        shorks += 100;
+        shorks += 1;
     }
 
     function incrementShorkCounter() {
-        if (standardForm == true) {
-            shorkCounter.textContent = formatInt(shorks);
-        } else {
+        if (formattingMode == 0) {
             shorkCounter.textContent = shorks.toLocaleString() + " Shorks";
+        } else if (formattingMode == 1) {
+            shorkCounter.textContent = formatInt(shorks);
+        } else if (formattingMode == 2) {
+            shorkCounter.textContent = formatIntAbbr(shorks);
+        } else if (formattingMode == 3) {
+            shorkCounter.textContent = shorks.toExponential(2) + " Shorks";
         }
     }
 
@@ -37,6 +51,15 @@
         return temp + " " + illions[illion - 1] + " Shorks";
     }
 
+    function formatIntAbbr(number) {
+        var temp = number;
+        var illion = 1;
+        while (temp >= 1000) {
+            temp = temp / 1000;
+            illion++;
+        }
+        return temp + illionsAbbr[illion - 1] + " Shorks";
+    }
     const everyTick = () => {
         incrementShorkCounter();
     }
