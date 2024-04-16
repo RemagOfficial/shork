a7fd8eb52d12995859e4cb05488dae1bdc252bdb
RemagOfficial <52126530+RemagOfficial@users.noreply.github.com>
2024-04-16 17:08:42 +0100

    added temp button to change formatting mode

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@
     let shorks = 0;
     const shorkCounter = document.getElementById("ShorksCount");
     const sharkImg = document.getElementById("Shark");
+    const formatting = document.getElementById("formatting");
 
     let standardForm = false;
 
     var illions = [" ", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion", "unvigintillion", "duovigintillion", "trevigintillion", "quattuorvigintillion", "quinvigintillion", "sexvigintillion", "septenvigintillion", "octovigintillion", "nonvigintillion", "trigintillion", "untrigintillion", "duotrigintillion", "tretrigintillion", "quattuortrigintillion", "quintrigintillion", "sextrigintillion", "septentrigintillion", "octotrigintillion", "novemtrigintillion", "quadragintillion", "unquadragintillion", "duoquadragintillion", "trequadragintillion", "quattuorquadragintillion", "quinquadragintillion", "sexquadragintillion", "septenquadragintillion", "octoquadragintillion", "novemquadragintillion", "quinquagintillion", "unquinquagintillion", "duoquinquagintillion", "trequinquagintillion"];
 
     sharkImg.addEventListener("click", incrementShorkCounter);
+    formatting.addEventListener("click", updateFormatting);
+
+    function updateFormatting() {
+        standardForm = !standardForm;
+    }
 
     function incrementShorkCounter() {
+        console.log(standardForm);
         shorks += 100;
         if (standardForm == true) {
             shorkCounter.textContent = formatInt(shorks);
