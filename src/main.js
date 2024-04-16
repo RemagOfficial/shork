b2ddac86b4b892b84cd36f3511ffc6f152f6a206
RemagOfficial <52126530+RemagOfficial@users.noreply.github.com>
2024-04-16 18:11:48 +0100

    a lot of things idk

--- src/main.js
+++ src/main.js
@@ -8,16 +8,18 @@
 
     var illions = [" ", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion", "unvigintillion", "duovigintillion", "trevigintillion", "quattuorvigintillion", "quinvigintillion", "sexvigintillion", "septenvigintillion", "octovigintillion", "nonvigintillion", "trigintillion", "untrigintillion", "duotrigintillion", "tretrigintillion", "quattuortrigintillion", "quintrigintillion", "sextrigintillion", "septentrigintillion", "octotrigintillion", "novemtrigintillion", "quadragintillion", "unquadragintillion", "duoquadragintillion", "trequadragintillion", "quattuorquadragintillion", "quinquadragintillion", "sexquadragintillion", "septenquadragintillion", "octoquadragintillion", "novemquadragintillion", "quinquagintillion", "unquinquagintillion", "duoquinquagintillion", "trequinquagintillion"];
 
-    sharkImg.addEventListener("click", incrementShorkCounter);
+    sharkImg.addEventListener("click", addShorks);
     formatting.addEventListener("click", updateFormatting);
 
     function updateFormatting() {
         standardForm = !standardForm;
     }
 
-    function incrementShorkCounter() {
-        console.log(standardForm);
+    function addShorks() {
         shorks += 100;
+    }
+
+    function incrementShorkCounter() {
         if (standardForm == true) {
             shorkCounter.textContent = formatInt(shorks);
         } else {
@@ -33,5 +35,12 @@
             illion++;
         }
         return temp + " " + illions[illion - 1] + " Shorks";
-        }
+    }
+
+    const everyTick = () => {
+        incrementShorkCounter();
+    }
+
+    setInterval(everyTick, 50); // Every 50ms = 1/20th of a second
+    
 })()
