962e45a7c65f91434478d0d10a6ce7868c223e0a
RemagOfficial <52126530+RemagOfficial@users.noreply.github.com>
2024-04-17 23:00:15 +0100

    added nav bar and about menu

--- src/main.js
+++ src/main.js
@@ -1,16 +1,38 @@
 (function() {
     let shorks = 0;
+
     const shorkCounter = document.getElementById("ShorksCount");
     const sharkImg = document.getElementById("Shark");
     const formatting = document.getElementById("formatting");
+    const body = document.getElementsByTagName("body")[0];
+    const title = document.getElementById("Title");
+    const about = document.getElementById("About");
+    const aboutPanel = document.getElementById("AboutDiv");
+    const close = document.getElementById("Close");
+    const save = document.getElementById("Save");
+    const load = document.getElementById("Load");
+
+    let hook = true;
 
     let formattingMode = 0;
 
     var illions = ["", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion", "unvigintillion", "duovigintillion", "trevigintillion", "quattuorvigintillion", "quinvigintillion", "sexvigintillion", "septenvigintillion", "octovigintillion", "nonvigintillion", "trigintillion", "untrigintillion", "duotrigintillion", "tretrigintillion", "quattuortrigintillion", "quintrigintillion", "sextrigintillion", "septentrigintillion", "octotrigintillion", "novemtrigintillion", "quadragintillion", "unquadragintillion", "duoquadragintillion", "trequadragintillion", "quattuorquadragintillion", "quinquadragintillion", "sexquadragintillion", "septenquadragintillion", "octoquadragintillion", "novemquadragintillion", "quinquagintillion", "unquinquagintillion", "duoquinquagintillion", "trequinquagintillion"];
     var illionsAbbr = ["", "k", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "O", "N", "D", "Ud", "DuD", "TrD", "QaD", "QiD", "SxD", "SpD", "OD", "ND", "V", "UnV", "DuV", "TrV", "QaV", "QiV", "SxV", "SpV", "OV", "NV", "Tg", "UnT", "DuT", "TrT", "QaT", "QiT", "SxT", "SpT", "OT", "NT", "Dg", "UnD", "DuDg", "TrDg", "QaDg", "QiDg", "SxDg", "SpDg", "ODg", "NDg", "Sg", "UnSg", "DuSg", "TrSg", "QaSg", "QiSg", "SxSg", "SpSg", "OSg"];
 
+
+    function handleBeforeUnload(event) {
+        if (hook) {
+            event.preventDefault();
+            event.returnValue = 'Warning: Game does not autosave. Use download button.';
+        }
+    }
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
     sharkImg.addEventListener("click", addShorks);
     formatting.addEventListener("click", updateFormatting);
+    about.addEventListener("click", openAbout);
+    close.addEventListener("click", closeAbout);
 
     /**
      * Updates the formatting mode of the application.
@@ -59,6 +81,21 @@
             illion++;
         }
         return temp + illionsAbbr[illion - 1] + " Shorks";
+    }
+
+    // a function that opens the about page by dimming the background and making the about panel div visible
+    function openAbout() {
+        body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        aboutPanel.style.visibility = "visible";
+    }
+
+    // a function that closes the about page by undimming the background and making the about panel div invisible
+    function closeAbout() {
+        body.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        aboutPanel.style.visibility = "hidden";
+    }
+    function logger() {
+        console.log("clicked");
     }
     const everyTick = () => {
         incrementShorkCounter();
